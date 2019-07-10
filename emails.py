a477eb1f058f9a7bcbdd307cb47dc293735d4b02
Jayakrishnan Damodaran <jakyjk05@gmail.com>
2019-07-11 00:09:51 +0530

    Added a dummy email service. Email service of choice can be injected into the consumer to send emails.

--- emails.py
+++ emails.py
@@ -9,33 +9,43 @@ from pika_client.base import Connector
 from pika_client.consumers import BasePubSubConsumer
 
 LOGGER = logging.getLogger(__name__)
+FROM_EMAIL = os.environ.get('FROM_EMAIL', 'no-reply@example.com')
 
 
 class EmailConsumer(BasePubSubConsumer):
-    pass
+    def __init__(
+            self,
+            service,
+            connector,
+            app_id='EMAIL_SERVICE',
+            queue='emails',
+            durable=False,
+            exchange='notifications_x',
+            exchange_type='topic',
+            routing_key='notifications.email'):
+        super().__init__(self, connector, app_id, queue, durable, exchange, exchange_type, routing_key)
+        self.service = service
+
+    def send_email(self, body):
+        body = json.loads(body)
+        recepients = body.get('recepients')
+        content = body.get('text')
+        self.service.send(recepients, content)
+
+
+class DummyMailService(object):
+    def send(self, recepients, body, from_email=FROM_EMAIL):
+        LOGGER.info("Mocking email.")
+        LOGGER.info("Recepients %s.", recepients)
+        LOGGER.info("Content is %s.", content)
+        LOGGER.info("From address %s.", from_email)
 
 
 def subscribe(connector):
-    consumer = EmailConsumer(
-        connector,
-        app_id='EMAIL_SERVICE',
-        queue='emails',
-        exchange='notifications_x',
-        exchange_type='topic',
-        routing_key='notifications.email'
-    )
-    consumer.register_callback('on_message', send_email)
+    consumer = EmailConsumer(service=DummyMailService(), connector=connector)
+    consumer.register_callback('on_message', consumer.send_email)
     LOGGER.info("Starting email consumer.")
     consumer.start()  # start listening infinitely.
-
-
-def send_email(body):
-    body = json.loads(body)
-    recepients = body.get('recepients')
-    content = body.get('text')
-    LOGGER.info("Mocking email.")
-    LOGGER.info("Recepients %s.", recepients)
-    LOGGER.info("Content is %s.", content)
 
 
 def main():
